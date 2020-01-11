06a9df2e7133ef1778f806bad278f5ded873c99f
Brian Hong <brian.hong@aofl.com>
2020-01-10 16:42:48 -0800

    complete up to mocking (unfinished)

--- 6-pointers/pointers_test.go
+++ 6-pointers/pointers_test.go
@@ -5,13 +5,53 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{5}
-	wallet.Deposit(10)
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
 
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw with funds", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		err := wallet.Withdraw(Bitcoin(5))
+
+		assertBalance(t, wallet, Bitcoin(15))
+		assertNoError(t, err)
+	})
+
+	t.Run("Withdraw insufficient funds", func(t *testing.T) {
+		startingFund := Bitcoin(5)
+		wallet := Wallet{startingFund}
+		err := wallet.Withdraw(Bitcoin(10))
+
+		assertBalance(t, wallet, startingFund)
+		assertError(t, err, "cannot withdraw, insufficent funds")
+	})
+}
+
+func assertBalance(t *testing.T, wallet Wallet, want Bitcoin) {
+	t.Helper()
 	got := wallet.Balance()
-	want := 15
 
 	if got != want {
-		t.Errorf("got %d want %d", got, want)
+		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func assertNoError(t *testing.T, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
+	}
+}
+
+func assertError(t *testing.T, got error, want string) {
+	t.Helper()
+	if got == nil {
+		t.Fatal("wanted error but didn't get one")
+	}
+	if got.Error() != want {
+		t.Errorf("got %q, want %q", got, want)
 	}
 }
---
+++ 9-mocking/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		time.Sleep(1 * time.Second)
+	}
+	fmt.Fprint(out, finalWord)
+}
+
+func main() {
+	defaultSleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, defaultSleeper)
+}
